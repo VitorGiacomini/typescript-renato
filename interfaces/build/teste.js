81a61b4f626ac3dd93e382852f5c5128ef14176d
Vitor <vitor.giacomini@estudante.ifms.edu.br>
2024-03-15 17:22:43 -0300

    second commit

---
+++ interfaces/build/teste.js
@@ -0,0 +1,32 @@
+"use strict";
+// Interface vazia
+// interface EmptyObject{}
+// // Interface Pessoa
+// interface Person{
+//     name: string;
+//     age: number;
+//     hairColor: string;
+//     weight: number;
+//     height: number;
+// }
+//Interface carro
+// interface Car {
+//     model: string;
+//     manufacturer: string;
+//     numberOfWheels: number;
+//     type: string;
+//   }
+//Interface user
+// interface User {
+//     name: string;
+//     pass: string;
+//     email: string;
+//   }
+// Interface Person com propriedades opcionais
+// interface Person {
+//     name: string;
+//     weight? : number;
+//     age: number;
+//     hairColor? : string;
+//      height: number;
+//   }
